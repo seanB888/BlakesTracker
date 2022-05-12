44f071d528e2f128cb5fcc0fbd0ae95d6523fcc0
Sean Blake <sean.blake97@outlook.com>
2022-05-12 17:36:53 -0400

    Initial Commit

---
+++ BlakesWallet/BlakesWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlakesWalletApp.swift
+//  BlakesWallet
+//
+//  Created by SEAN BLAKE on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BlakesWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
