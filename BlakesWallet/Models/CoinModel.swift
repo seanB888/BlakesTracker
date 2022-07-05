977689275c1028f1f4034bbdf5e05bff5488e392
Sean Blake <sean.blake97@outlook.com>
2022-07-04 23:46:36 -0400

    Fixed issue with chart not showing, sparkline was false, switch to true

--- BlakesWallet/Models/CoinModel.swift
+++ BlakesWallet/Models/CoinModel.swift
@@ -10,7 +10,7 @@ import Foundation
 // CoinGecko API info
 /*
  URL:
- https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h
+ https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
 
  JSON Response:
  {
--- BlakesWallet/Services/CoinDataServices.swift
+++ BlakesWallet/Services/CoinDataServices.swift
@@ -19,7 +19,7 @@ class CoinDataServices {
 
     func getCoins() {
         // get request
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h")
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
 
         // Combine formula...
